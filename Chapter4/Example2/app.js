47a86d32ed63a7dddba033ed5c6c70ae552cc95e
DiazHub <edwin92diaz@gmail.com>
2016-09-26 18:10:29 -0700

    Added index app and style files

--- Chapter4/Example2/app.js
+++ Chapter4/Example2/app.js
@@ -1,27 +1,54 @@
+/* jshint browser: true, jquery: true, camelcase: true, indent: 2, undef: true, quotmark: single, maxlen: 80, trailing: true, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, nonew: true, unused: true, strict: true */
 var main = function () {
-    "use strict";
+    'use strict';
 
-    var addCommentFromInputBox = function () {
-        var $new_comment;
+    var addCommentFromInputBox = function (user) {
+        var $newComment;
+      
+        if(user === 'left-user'){
 
-        if ($(".comment-input input").val() !== "") {
-            $new_comment = $("<p>").text($(".comment-input input").val());
-            $new_comment.hide();
-            $(".comments").append($new_comment);
-            $new_comment.fadeIn();
-            $(".comment-input input").val("");
+            if ($('.left-comment-input input').val() !== '') {
+                $newComment = $('<p>').addClass('left-user')
+                .text($('.left-comment-input input').val());
+                $newComment.hide();
+                $('.comments').append($newComment);
+                $newComment.fadeIn();
+                $('.left-comment-input input').val('');
+            }
         }
+
+        if(user === 'right-user') {
+            if ($('.right-comment-input input').val() !== '') {
+                $newComment = $('<p>').addClass('right-user')
+                .text($('.right-comment-input input').val());
+                $newComment.hide();
+                $('.comments').append($newComment);
+                $newComment.fadeIn();
+                $('.right-comment-input input').val('');
+            }
+        }
+
     };
 
-    $(".comment-input button").on("click", function (event) {
-        addCommentFromInputBox();
+    $('.left-comment-input button').on('click', function () {
+        addCommentFromInputBox('left-user');
     });
 
-    $(".comment-input input").on("keypress", function (event) {
+    $('.right-comment-input button').on('click', function () {
+        addCommentFromInputBox('right-user');
+    });
+
+    $('.left-comment-input input').on('keypress', function () {
         if (event.keyCode === 13) {
-            addCommentFromInputBox();
+            addCommentFromInputBox('left-user');
         }
     });
+
+    $('.right-comment-input input').on('keypress', function () {
+        if (event.keyCode === 13) {
+            addCommentFromInputBox('right-user');
+        }
+    });    
 };
 
 $(document).ready(main);
